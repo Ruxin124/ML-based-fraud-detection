807755e0754224336173af735fefe48d2efe27ee
Cecile Zheng <cecilezheng66@gmail.com>
2024-01-25 15:03:23 +0100

    fraud detection first version done

---
+++ my_function.py
@@ -0,0 +1,101 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.metrics import r2_score, classification_report, confusion_matrix, accuracy_score, roc_auc_score, roc_curve, precision_recall_curve, average_precision_score
+from sklearn.metrics import homogeneity_score, silhouette_score
+from itertools import product
+
+def prep_data(df: pd.DataFrame) -> (np.ndarray, np.ndarray):
+    """
+    Convert the DataFrame into two variable
+    X: data columns (V1 - V28)
+    y: lable column
+    """
+    X = df.iloc[:, 2:30].values
+    y = df.Class.values
+    return X, y
+
+def compare_plot(X: np.ndarray, y: np.ndarray, X_resampled: np.ndarray, y_resampled: np.ndarray, method: str):
+    plt.subplot(1, 2, 1)
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='r')
+    plt.title('Original Set')
+    plt.subplot(1, 2, 2)
+    plt.scatter(X_resampled[y_resampled == 0, 0], X_resampled[y_resampled == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
+    plt.scatter(X_resampled[y_resampled == 1, 0], X_resampled[y_resampled == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='r')
+    plt.title(method)
+    plt.legend()
+    plt.show()
+
+# Define a function to create a scatter plot of our data and labels
+def plot_data(X: np.ndarray, y: np.ndarray):
+    plt.scatter(X[y == 0, 0], X[y == 0, 1], label="Class #0", alpha=0.5, linewidth=0.15)
+    plt.scatter(X[y == 1, 0], X[y == 1, 1], label="Class #1", alpha=0.5, linewidth=0.15, c='r')
+    plt.legend()
+    return plt.show()
+
+
+def get_model_results(X_train: np.ndarray, y_train: np.ndarray,
+                      X_test: np.ndarray, y_test: np.ndarray, model):
+    """
+    model: sklearn model (e.g. RandomForestClassifier)
+    """
+    
+    # Fit your training model to your training set
+    model.fit(X_train, y_train)
+
+    # Obtain the predicted values and probabilities from the model 
+    predicted = model.predict(X_test)
+    
+    try:
+        probs = model.predict_proba(X_test)
+        print('ROC Score:')
+        print(roc_auc_score(y_test, probs[:,1]))
+    except AttributeError:
+        pass
+
+
+    # Print the ROC curve, classification report and confusion matrix
+    print('\nClassification Report:')
+    print(classification_report(y_test, predicted))
+    print('\nConfusion Matrix:')
+    print(confusion_matrix(y_test, predicted))
+
+
+def plot_confusion_matrix(cm, classes=['Not Fraud', 'Fraud'],
+                          normalize=False,
+                          title='Fraud Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    From:
+        http://scikit-learn.org/stable/auto_examples/model_selection/plot_confusion_matrix.html#sphx-glr-auto-
+        examples-model-selection-plot-confusion-matrix-py
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    # print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.show()
